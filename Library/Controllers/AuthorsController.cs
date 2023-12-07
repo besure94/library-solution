4df9a329ef6eb3c3e79e12b7f15061b7f343ce14
Brian Scherner <brianscherner@gmail.com>
2023-12-07 09:49:39 -0800

    Add functionality to add multiple books to an author.

--- Library/Controllers/AuthorsController.cs
+++ Library/Controllers/AuthorsController.cs
@@ -44,8 +44,32 @@ namespace Library.Controllers
 
     public ActionResult Details(int id)
     {
-      Author thisAuthor = _db.Authors.FirstOrDefault(author => author.AuthorId == id);
+      Author thisAuthor = _db.Authors
+      .Include(author => author.JoinEntities)
+      .ThenInclude(author => author.Book)
+      .FirstOrDefault(author => author.AuthorId == id);
       return View(thisAuthor);
+    }
+
+    public ActionResult AddBook(int id)
+    {
+      Author thisAuthor = _db.Authors.FirstOrDefault(authors => authors.AuthorId == id);
+      ViewBag.BookId = new SelectList(_db.Books, "BookId", "Title");
+      return View(thisAuthor);
+    }
+
+    [HttpPost]
+    public ActionResult AddBook(Author author, int bookId)
+    {
+      #nullable enable
+      AuthorBook? joinEntity = _db.AuthorBooks.FirstOrDefault(join => join.AuthorBookId == bookId && join.AuthorId == author.AuthorId);
+      #nullable disable
+      if (joinEntity == null && bookId != 0)
+      {
+        _db.AuthorBooks.Add(new AuthorBook() { BookId = bookId, AuthorId = author.AuthorId });
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Details", new { id = author.AuthorId });
     }
 
   }
