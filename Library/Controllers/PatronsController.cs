f3043efe3bd43fbbb2e84bf3986425aaeed02565
Brian Scherner <brianscherner@gmail.com>
2023-12-12 12:26:23 -0800

    Add functionality to create a patron, list patrons and view their details.

---
+++ Library/Controllers/PatronsController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using Library.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Library.Controllers
+{
+  public class PatronsController : Controller
+  {
+    private readonly LibraryContext _db;
+    public PatronsController(LibraryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Patrons.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Patron patron)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View(patron);
+      }
+      else
+      {
+        _db.Patrons.Add(patron);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+      }
+    }
+
+    public ActionResult Details(int id)
+    {
+      Patron thisPatron = _db.Patrons.FirstOrDefault(patron => patron.PatronId == id);
+      return View(thisPatron);
+    }
+  }
+}
