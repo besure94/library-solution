73376163cac9d8b28e788b96b9a455473b075fb6
Brian Scherner <brianscherner@gmail.com>
2023-12-12 12:20:39 -0800

    Add Patron model and update DB.

---
+++ Library/Migrations/20231212201953_AddPatronTable.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Library.Migrations
+{
+    public partial class AddPatronTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Patrons",
+                columns: table => new
+                {
+                    PatronId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    PhoneNumber = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patrons", x => x.PatronId);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Patrons");
+        }
+    }
+}
---
+++ Library/Models/Patron.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+  public class Patron
+  {
+    public int PatronId { get; set; }
+
+    [Required(ErrorMessage = "You must provide a name.")]
+    public string Name { get; set; }
+
+    [Required(ErrorMessage = "You must provide a phone number.")]
+    [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone number")]
+    public string PhoneNumber { get; set; }
+    // public List<Checkout> JoinEntities { get; set; }
+
+  }
+}
